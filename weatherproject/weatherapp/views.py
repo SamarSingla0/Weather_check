c1aca393c646e32708472a36a0efda05805f5988
SamarSingla0 <samarsingla04@gmail.com>
2025-11-25 19:10:56 +0530

    completed

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,35 +1,68 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.contrib import messages
 import requests
 import datetime
 
+
 def home(request):
 
-    if request.method == "POST":
-        city = request.POST.get('city')
+    # City detection
+    if 'city' in request.POST:
+        city = request.POST['city']
     else:
-        city = "Bangalore"
+        city = 'indore'
 
-    API_KEY = "5b6a9e5227f8afbbf5a2c3a03de2b1bb"
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    WEATHER_API_KEY = ''
+    SEARCH_ENGINE_ID = ''
 
-    response = requests.get(url)
-    
-    if response.status_code != 200:
-        context = {"error": "City not found", "city": city}
-        return render(request, "weatherapp/home.html", context)
+    # WEATHER API URL
+    weather_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}'
+    PARAMS = {'units': 'metric'}
 
-    data = response.json()
+    # GOOGLE IMAGE SEARCH URL
+    query = f"{city} 1920x1080"
+    google_url = (
+        f"https://www.googleapis.com/customsearch/v1?"
+        f"key={WEATHER_API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}"
+        f"&searchType=image&imgSize=xlarge"
+    )
 
-    description = data["weather"][0]["description"]
-    temp = data["main"]["temp"]
-    icon = data["weather"][0]["icon"]
-    day = datetime.datetime.today()
-    
-    context = {
-        "day": day,
-        "description": description,
-        "temp": temp,
-        "icon": icon,
-    }
+    # IMAGE FETCH
+    try:
+        img_data = requests.get(google_url).json()
+        image_url = img_data["items"][0]["link"]
+    except:
+        image_url = None
 
-    return render(request, "weatherapp/home.html", context)
+    # WEATHER FETCH
+    try:
+        data = requests.get(weather_url, params=PARAMS).json()
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+        day = datetime.date.today()
+
+        return render(request, 'weatherapp/index.html', {
+            'description': description,
+            'icon': icon,
+            'temp': temp,
+            'day': day,
+            'city': city,
+            'image_url': image_url,
+            'exception_occurred': False,
+        })
+
+    except KeyError:
+        messages.error(request, "City not found. Showing default Indore weather.")
+
+        day = datetime.date.today()
+
+        return render(request, 'weatherapp/index.html', {
+            'description': 'clear sky',
+            'icon': '01d',
+            'temp': 25,
+            'day': day,
+            'city': 'indore',
+            'image_url': image_url,
+            'exception_occurred': True,
+        })
